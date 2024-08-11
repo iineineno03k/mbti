ac23686cb37b13b0c8924bccf813b0612c832ad8
いね <iineineno03k@gmail.com>
2024-08-11 20:45:31 +0900

    ユーザー型作成

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"backend/constant"
 	"fmt"
 )
 
+type User struct {
+	LastName  string
+	FirstName string
+	Nickname  string
+	MBTI      constant.MBTI
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	user := User{
+		LastName:  "Suzuki",
+		FirstName: "Taro",
+		Nickname:  "Suzu",
+		MBTI:      constant.INTJ_A,
+	}
+
+	fmt.Printf(user.Nickname)
 }
