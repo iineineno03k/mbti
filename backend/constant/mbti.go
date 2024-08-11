1775293f5d7e29357811641b3b82de16415cc90d
いね <iineineno03k@gmail.com>
2024-08-11 20:45:15 +0900

    MBTI定数を作成

---
+++ backend/constant/mbti.go
@@ -0,0 +1,38 @@
+package constant
+
+type MBTI int
+
+const (
+	INTJ_A MBTI = iota // 0
+	INTJ_T
+	INTP_A
+	INTP_T
+	ENTJ_A
+	ENTJ_T
+	ENTP_A
+	ENTP_T
+	INFJ_A
+	INFJ_T
+	INFP_A
+	INFP_T
+	ENFJ_A
+	ENFJ_T
+	ENFP_A
+	ENFP_T
+	ISTJ_A
+	ISTJ_T
+	ISFJ_A
+	ISFJ_T
+	ESTJ_A
+	ESTJ_T
+	ESFJ_A
+	ESFJ_T
+	ISTP_A
+	ISTP_T
+	ISFP_A
+	ISFP_T
+	ESTP_A
+	ESTP_T
+	ESFP_A
+	ESFP_T
+)
