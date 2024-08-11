8aee9333a2617460c3c257464c4390d04ff0c38c
いね <iineineno03k@gmail.com>
2024-08-11 21:03:05 +0900

    ユーザー取得APIのモック作成

--- backend/main.go
+++ backend/main.go
@@ -2,23 +2,47 @@ package main
 
 import (
 	"backend/constant"
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 type User struct {
+	Id        int
 	LastName  string
 	FirstName string
 	Nickname  string
 	MBTI      constant.MBTI
 }
 
-func main() {
-	user := User{
-		LastName:  "Suzuki",
-		FirstName: "Taro",
-		Nickname:  "Suzu",
+func getAllUsers(c echo.Context) error {
+	user1 := User{
+		Id:        1,
+		LastName:  "田中",
+		FirstName: "太郎",
+		Nickname:  "たなっち",
 		MBTI:      constant.INTJ_A,
 	}
+	user2 := User{
+		Id:        2,
+		LastName:  "山田",
+		FirstName: "花子",
+		Nickname:  "はなちゃん",
+		MBTI:      constant.ENFJ_T,
+	}
 
-	fmt.Printf(user.Nickname)
+	var users = []User{
+		user1,
+		user2,
+	}
+
+	return c.JSON(http.StatusOK, users)
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/user", getAllUsers)
+
+	e.Logger.Fatal(e.Start(":8080"))
+
 }
